9b4a3c1587fc4a79862a85b5ffc0d9d74db93ade
DanielNDA <nitu.daniel93@yahoo.ro>
2020-10-10 12:14:20 +0300

    no message

---
+++ onlinestore-backend/src/main/java/com/sda/onlinestore/service/ProductService.java
@@ -0,0 +1,32 @@
+package com.sda.onlinestore.service;
+
+import com.sda.onlinestore.model.ProductModel;
+import com.sda.onlinestore.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void save(ProductModel productModel){
+        productRepository.save(productModel);
+    }
+
+    public void deleteById(Long id){
+        productRepository.deleteById(id);
+    }
+
+    public List<ProductModel> findAll(){
+      return   productRepository.findAll();
+    }
+
+    public void update(ProductModel productModel){
+        productRepository.save(productModel);
+    }
+
+}
