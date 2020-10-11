299dc9356bd3d78626b773dabff1dd30641928b1
alisaalexandraa <aliciousx@yahoo.com>
2020-10-11 11:09:36 +0300

    no message

--- onlinestore-backend/src/main/java/com/sda/onlinestore/dto/OrderDTO.java
+++ onlinestore-backend/src/main/java/com/sda/onlinestore/dto/OrderDTO.java
@@ -1,15 +1,11 @@
 package com.sda.onlinestore.dto;
 
-import java.util.Date;
 import java.util.List;
 
 public class OrderDTO {
 
     private Long id;
-    private String userName;
     private Double total;
-    private Date dateOfOrder;
-    private String status;
     private List<OrderLineDTO> orderLines;
 
     public Long getId() {
@@ -20,36 +16,12 @@ public class OrderDTO {
         this.id = id;
     }
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
     public Double getTotal() {
         return total;
     }
 
     public void setTotal(Double total) {
         this.total = total;
-    }
-
-    public Date getDateOfOrder() {
-        return dateOfOrder;
-    }
-
-    public void setDateOfOrder(Date dateOfOrder) {
-        this.dateOfOrder = dateOfOrder;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
     }
 
     public List<OrderLineDTO> getOrderLines() {
