2ffa970b52cccea084db529afc2f374deb70ecf1
alisaalexandraa <aliciousx@yahoo.com>
2020-10-11 11:31:06 +0300

    no message

--- onlinestore-backend/src/main/java/com/sda/onlinestore/dto/OrderLineDTO.java
+++ onlinestore-backend/src/main/java/com/sda/onlinestore/dto/OrderLineDTO.java
@@ -4,6 +4,37 @@ public class OrderLineDTO {
     private Long id;
     private Integer quantity;
     private Double price;
-    //private ProductDTO productDTO;
-    
+    private ProductDto productDTO;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public ProductDto getProductDTO() {
+        return productDTO;
+    }
+
+    public void setProductDTO(ProductDto productDTO) {
+        this.productDTO = productDTO;
+    }
 }
