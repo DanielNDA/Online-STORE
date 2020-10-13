64fa135f290a429970a54272aa60bad5af66b1c1
alisaalexandraa <aliciousx@yahoo.com>
2020-10-13 19:57:35 +0300

    no message

--- onlinestore-frontend/src/app/app.module.ts
+++ onlinestore-frontend/src/app/app.module.ts
@@ -8,12 +8,26 @@ import { UserEditComponent } from './users/components/user-edit/user-edit.compon
 import { RegisterComponent } from './users/components/register/register.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {OrderListComponent} from './orders/components/order-list/order-list.component';
+import {OrderAddComponent} from './orders/components/order-add/order-add.component';
+import {OrderUsersComponent} from './orders/components/order-users/order-users.component';
+import {ProductAddComponent} from './products/components/product-add/product-add.component';
+import {ProductEditComponent} from './products/components/product-edit/product-edit.component';
+import {ProductListComponent} from './products/components/product-list/product-list.component';
+import {ProductViewComponent} from './products/components/product-view/product-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserEditComponent,
-    RegisterComponent
+    RegisterComponent,
+    OrderListComponent,
+    OrderAddComponent,
+    OrderUsersComponent,
+    ProductAddComponent,
+    ProductEditComponent,
+    ProductListComponent,
+    ProductViewComponent
   ],
   imports: [
     BrowserModule,
