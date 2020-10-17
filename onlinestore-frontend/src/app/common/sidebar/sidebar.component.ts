d9cc6eaff95950619090a7697ce10e9d5b7ac6c9
Hamericano <m_glisca99@yahoo.com>
2020-10-17 11:15:43 -0700

    no message

--- onlinestore-frontend/src/app/common/sidebar/sidebar.component.ts
+++ onlinestore-frontend/src/app/common/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  redirectToAddCategory() {
+    this.router.navigate(['category-add']);
   }
 
 }
