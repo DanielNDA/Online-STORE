30324e1f103058b2a14db89f82c44fb65ef10627
alisaalexandraa <aliciousx@yahoo.com>
2020-10-23 19:34:10 +0300

    no message

--- onlinestore-frontend/src/app/app.module.ts
+++ onlinestore-frontend/src/app/app.module.ts
@@ -7,13 +7,11 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {UserEditComponent} from './users/components/user-edit/user-edit.component';
 import {RegisterComponent} from './users/components/register/register.component';
 import {FormsModule} from '@angular/forms';
-<<<<<<< Updated upstream
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {OrderListComponent} from './orders/components/order-list/order-list.component';
-=======
+
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {HttpClientModule} from '@angular/common/http';
 import {OrderComponent} from './orders/components/order/order.component';
->>>>>>> Stashed changes
+
 import {OrderAddComponent} from './orders/components/order-add/order-add.component';
 import {OrderUsersComponent} from './orders/components/order-users/order-users.component';
 import {ProductAddComponent} from './products/components/product-add/product-add.component';
@@ -25,7 +23,7 @@ import {HomeComponent} from './common/home/home.component';
 import {NavbarComponent} from './common/navbar/navbar.component';
 import {FooterComponent} from './common/footer/footer.component';
 import {CategoryListComponent} from './categories/components/category-list/category-list.component';
-<<<<<<< Updated upstream
+
 import {UserViewComponent} from './users/components/user-view/user-view.component';
 import {CategoryEditComponent} from './categories/components/category-edit/category-edit.component';
 import {LoginComponent} from './users/components/login/login.component';
@@ -42,9 +40,7 @@ import {HttpInterceptorService} from './users/service/http-interceptor.service';
 import {PrivilegeEditComponent} from './security/privileges/components/privilege-edit/privilege-edit.component';
 import {PrivilegeFormComponent} from './security/privileges/components/privilege-form/privilege-form.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
-=======
 import {NgxPaginationModule} from 'ngx-pagination';
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -62,7 +58,6 @@ import {NgxPaginationModule} from 'ngx-pagination';
     HomeComponent,
     NavbarComponent,
     FooterComponent,
-<<<<<<< Updated upstream
     CategoryListComponent,
     UserViewComponent,
     CategoryEditComponent,
@@ -83,6 +78,7 @@ import {NgxPaginationModule} from 'ngx-pagination';
     NgbModule,
     FormsModule,
     HttpClientModule,
+    NgxPaginationModule
   ],
   providers: [OrderService, ProductService, UserService,
     {
@@ -90,19 +86,6 @@ import {NgxPaginationModule} from 'ngx-pagination';
       useClass: HttpInterceptorService,
       multi: true
     }],
-=======
-    CategoryListComponent
-  ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        NgbModule,
-        FormsModule,
-        HttpClientModule,
-        NgxPaginationModule,
-    ],
-  providers: [],
->>>>>>> Stashed changes
   bootstrap: [AppComponent]
 })
 export class AppModule {
