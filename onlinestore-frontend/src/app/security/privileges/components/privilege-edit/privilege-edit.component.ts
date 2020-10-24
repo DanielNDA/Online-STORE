4e918b5a5b3270362716fb147e5cfcff840859ff
Hamericano <m_glisca99@yahoo.com>
2020-10-24 09:44:04 -0700

    no message

--- onlinestore-frontend/src/app/security/privileges/components/privilege-edit/privilege-edit.component.ts
+++ onlinestore-frontend/src/app/security/privileges/components/privilege-edit/privilege-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Privilege} from '../../model/privilege';
+import {ActivatedRoute, Router} from '@angular/router';
+import {PrivilegeService} from '../../service/privilege.service';
 
 @Component({
   selector: 'app-privilege-edit',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrivilegeEditComponent implements OnInit {
 
-  constructor() { }
+  privilege: Privilege;
+  id: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private privilegeService: PrivilegeService) {
+    this.privilege = new Privilege();
+  }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params.id;
+    this.privilegeService.getById(this.id).subscribe(data => {
+      this.privilege = new Privilege();
+      this.privilege = data;
+    });
+  }
+
+  // tslint:disable-next-line:typedef
+  onSubmit() {
+    this.privilegeService.update(this.privilege).subscribe(result => this.goToPrivilegeList());
+  }
+
+  // tslint:disable-next-line:typedef
+  goToPrivilegeList() {
+    this.router.navigate(['/privilege']);
   }
 
 }
--- onlinestore-frontend/src/app/security/roles/components/role-form/role-form.component.ts
+++ onlinestore-frontend/src/app/security/roles/components/role-form/role-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Role} from '../../model/role';
+import {ActivatedRoute, Router} from '@angular/router';
+import {RoleService} from '../../service/role.service';
 
 @Component({
   selector: 'app-role-form',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoleFormComponent implements OnInit {
 
-  constructor() { }
+  role: Role;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private roleService: RoleService) {
+    this.role = new Role();
+  }
+
+  // tslint:disable-next-line:typedef
+  onSubmit() {
+    this.roleService.save(this.role).subscribe(result => this.goToRoleList());
+  }
+
+  // tslint:disable-next-line:typedef
+  goToRoleList() {
+    this.router.navigate(['/role']);
+  }
 
   ngOnInit(): void {
   }
-
 }
